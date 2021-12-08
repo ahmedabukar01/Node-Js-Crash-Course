75d33b1491d36fa5f3ce704c6c8f8596b6477405
ahmed <ahmedlearn01@gmail.com>
2021-12-08 10:55:20 -0800

    initial commit

--- file.js
+++ file.js
@@ -1,16 +1,44 @@
 const fs = require('fs');
+const { fileURLToPath } = require('url');
 
 // read file
-fs.readFile('./docs/blog1.txt', (error,data)=>{
-    if(error){
-        console.log(error);
-    }
 
-    console.log(data.toString());
-})
+// fs.readFile('./docs/blog1.txt', (error,data)=>{
+//     if(error){
+//         console.log(error);
+//     }
+
+//     console.log(data.toString());
+// })
 
 // write file
+fs.writeFile('./docs/blog1.txt',"hello ahmed the warrior", ()=>{
+    console.log("file written")
+})
 
 // redirect file
+if (!fs.existsSync('assets')){
+    fs.mkdir('./assets', (err)=>{
+        if(err){
+            console.log(err);
+        }
+
+        console.log('folder created');
+    })
+} else{
+    fs.rmdir('./assets', ()=>{
+        console.log('file removed')
+    })
+}
 
 // delete file
+if(fs.existsSync('deleted.txt')){
+    fs.unlinkSync('./docs/deleteme.txt',(err)=>{
+        if(err){
+            console.log(err);
+        }
+    
+        console.log('file deleted')
+    
+    })
+}
