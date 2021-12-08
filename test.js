f1b5965267cb68d4e71c1b726f4a3b7e1cd18fcb
ahmed <ahmedlearn01@gmail.com>
2021-12-08 07:35:40 -0800

    initial node

---
+++ test.js
@@ -0,0 +1,12 @@
+
+
+let name = "ahmed the warrior";
+
+setTimeout(()=>{
+    console.log(`Hello ${name}`);
+    clearInterval(int)
+},3000);
+
+const int = setInterval(()=>{
+    console.log('in the interval...');
+},1000);
