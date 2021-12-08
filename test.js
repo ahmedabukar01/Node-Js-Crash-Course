65064872bbaac67dd5483f31636026910af36373
ahmed <ahmedlearn01@gmail.com>
2021-12-08 07:45:52 -0800

    initial commit

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 
 
-let name = "ahmed the warrior";
+// let name = "ahmed the warrior";
 
-setTimeout(()=>{
-    console.log(`Hello ${name}`);
-    clearInterval(int)
-},3000);
+// setTimeout(()=>{
+//     console.log(`Hello ${name}`);
+//     clearInterval(int)
+// },3000);
 
-const int = setInterval(()=>{
-    console.log('in the interval...');
-},1000);
+// const int = setInterval(()=>{
+//     console.log('in the interval...');
+// },1000);
+
+
+console.log(__dirname)
+console.log(__filename)
