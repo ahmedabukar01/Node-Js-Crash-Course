30c02a83ca81cb265f78868f8968956033ccdb13
ahmed <ahmedlearn01@gmail.com>
2021-12-12 12:18:22 -0800

    view engines

--- express.js
+++ express.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const fs = require('fs');
-const { escapeRegExp } = require('lodash');
+const _ = require('lodash')
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','myviews')
 
 app.listen('3000',()=>{
     console.log('express request has been made :)');
 })
 
-app.get('/',(req,res)=>{
-    res.sendFile('./view/index.html',{root: __dirname});
+
+// normal GET request 
+
+// app.get('/',(req,res)=>{
+//     res.sendFile('./view/index.html',{root: __dirname});
+// })
+
+app.get('/', (req,res)=>{
+    res.render('index');
 })
 
 app.get('/about',(req,res)=>{
-   res.sendFile('./view/about.html', {root: __dirname});
+   res.render('about');
 })
 
-// redirects
-app.get('/aboutus',(req,res)=>{
-    res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{
+    res.render('create');
 })
 
 // 404 erros
 app.use((req,res)=>{
-    res.status(404).sendFile('./view/404.html',{root: __dirname});
+    res.status(404).render('404');
 })
