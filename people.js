d5a31d23bb7d6f37926170ad193fa01795b38378
ahmed <ahmedlearn01@gmail.com>
2021-12-13 10:15:36 -0800

    morning test

--- people.js
+++ people.js
@@ -1,8 +1,15 @@
-const people = ["ahmed","aubkar","omar"];
-const ages = [12,29,90];
+// const people = ["ahmed","aubkar","omar"];
+// const ages = [12,29,90];
 
-console.log(people);
+// console.log(people);
+
+// module.exports = {
+//     people, ages
+// }
+
+const people = ['ahmed','cali','rooble'];
+const ages = [23,44,32,54,32];
 
 module.exports = {
-    people, ages
+    people,ages
 }
--- test.js
+++ test.js
@@ -5,12 +5,79 @@
 // setTimeout(()=>{
 //     console.log(`Hello ${name}`);
 //     clearInterval(int)
-// },3000);
+// },1000);
 
 // const int = setInterval(()=>{
 //     console.log('in the interval...');
 // },1000);
 
+const fs = require('fs');
+const http = require('http');
 
-console.log(__dirname)
-console.log(__filename)
+// fs.writeFile('./nor.txt','hey weredddd are writing something', (err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log('aldkfjkl')
+//     }
+// })
+
+// fs.readFile('./nor.txt',{'encoding': 'utf-8'},(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     } else{
+//         console.log(data)
+//     }
+// })
+
+// if(!fs.existsSync('./assets')){
+//     fs.mkdir('./assets',(err)=>{
+//         if(err){
+//             console.log(err)
+//         } else{
+//             console.log('folder created');
+//         }
+//     })
+   
+// } else{
+//     fs.rmdir('./assets',(err)=>{
+//         if(err){
+//             console.log(err)
+//         } else{
+//             console.log('folder removed')
+//         }
+//     })
+// }
+
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url)
+//     res.setHeader('content-type', 'text/plain')
+//     res.write('hey')
+    
+
+//     if(req.url === '/me'){
+//         res.write('yes of course')
+//         res.end()
+//     }
+
+//     console.log('meeeeddaldkfj',req.url);
+
+// })
+
+// server.listen('3000','localhost',()=>{
+//     console.log('request is bein made')
+// })
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.sendFile('./myviews/html/index.html',{root: __dirname})
+})
+app.get('/us',(req,res)=>{
+ res.redirect('/')
+})
+
+app.listen('3000')
